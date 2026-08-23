a5d8339441ad94b712e61b249c1ebb4f7fd0f6a9
Joe <joe@showblender.com>
2026-08-23 11:59:35 -0700

    Initial Commit

---
+++ ViewLens/ViewLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewLensApp.swift
+//  ViewLens
+//
+//  Created by Joseph McCraw on 8/23/26.
+//
+
+import SwiftUI
+
+@main
+struct ViewLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ViewLensTests/ViewLensTests.swift
@@ -0,0 +1,19 @@
+//
+//  ViewLensTests.swift
+//  ViewLensTests
+//
+//  Created by Joseph McCraw on 8/23/26.
+//
+
+import Testing
+@testable import ViewLens
+
+struct ViewLensTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
